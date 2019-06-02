40378fc59bcae2828b8a39b101c4cbfa08532b11
oraant <oraant777@gmail.com>
2019-06-02 16:55:50 +0800

    Upgrade Hotkey module, now others can register callback function into specific key.

--- bin/hotkeys.py
+++ bin/hotkeys.py
@@ -2,98 +2,107 @@ import threading
 import ctypes
 import ctypes.wintypes
 from win32con import *
-from bin import speaker, common
+from bin import common
 
 user32 = ctypes.windll.user32  # Windows下的user32.dll
 
 class Hotkey(threading.Thread):  # 通过热键操作某个数值的大小
 
-    # 热键被调用时默认的方法
-    def f(self): common.log("调用了一个未被注册的热键", 20)
+    # 管理热键对应的回调函数
+    Mapping = {
+        # http://www.kbdedit.com/manual/low_level_vk_list.html
 
-    # 热键和方法的绑定映射
-    Mapping = {  # http://www.kbdedit.com/manual/low_level_vk_list.html
-        MOD_WIN:{
-            VK_NUMPAD1: f, VK_NUMPAD2: f, VK_NUMPAD3: f, VK_NUMPAD4: f, VK_NUMPAD5: f,
-            VK_NUMPAD6: f, VK_NUMPAD7: f, VK_NUMPAD8: f, VK_NUMPAD9: f, VK_NUMPAD0: f,
-            VK_DECIMAL: f, VK_ADD: f, VK_SUBTRACT: f, VK_MULTIPLY: f, VK_DIVIDE: f, VK_RETURN: f,
-        },
-        MOD_SHIFT: {
-            VK_NUMPAD1: f, VK_NUMPAD2: f, VK_NUMPAD3: f, VK_NUMPAD4: f, VK_NUMPAD5: f,
-            VK_NUMPAD6: f, VK_NUMPAD7: f, VK_NUMPAD8: f, VK_NUMPAD9: f, VK_NUMPAD0: f,
-            VK_DECIMAL: f, VK_ADD: f, VK_SUBTRACT: f, VK_MULTIPLY: f, VK_DIVIDE: f, VK_RETURN: f,
-        },
-        MOD_CONTROL: {
-            VK_NUMPAD1: f, VK_NUMPAD2: f, VK_NUMPAD3: f, VK_NUMPAD4: f, VK_NUMPAD5: f,
-            VK_NUMPAD6: f, VK_NUMPAD7: f, VK_NUMPAD8: f, VK_NUMPAD9: f, VK_NUMPAD0: f,
-            VK_DECIMAL: f, VK_ADD: f, VK_SUBTRACT: f, VK_MULTIPLY: f, VK_DIVIDE: f, VK_RETURN: f,
-        },
-        MOD_ALT: {
-            VK_NUMPAD1: f, VK_NUMPAD2: f, VK_NUMPAD3: f, VK_NUMPAD4: f, VK_NUMPAD5: f,
-            VK_NUMPAD6: f, VK_NUMPAD7: f, VK_NUMPAD8: f, VK_NUMPAD9: f, VK_NUMPAD0: f,
-            VK_DECIMAL: f, VK_ADD: f, VK_SUBTRACT: f, VK_MULTIPLY: f, VK_DIVIDE: f, VK_RETURN: f,
-        },
+        # Win
+        100:[MOD_WIN, VK_NUMPAD0, None], 101:[MOD_WIN, VK_NUMPAD1, None], 102:[MOD_WIN, VK_NUMPAD2, None],
+        103:[MOD_WIN, VK_NUMPAD3, None], 104:[MOD_WIN, VK_NUMPAD4, None], 105:[MOD_WIN, VK_NUMPAD5, None],
+        106:[MOD_WIN, VK_NUMPAD6, None], 107:[MOD_WIN, VK_NUMPAD7, None], 108:[MOD_WIN, VK_NUMPAD8, None],
+        109:[MOD_WIN, VK_NUMPAD9, None],
+
+        110:[MOD_WIN, VK_DECIMAL, None], 111:[MOD_WIN, VK_ADD, None], 112:[MOD_WIN, VK_SUBTRACT, None],
+        113:[MOD_WIN, VK_MULTIPLY, None], 114:[MOD_WIN, VK_DIVIDE, None], 115:[MOD_WIN, VK_RETURN, None],
+
+        # Shift
+        200: [MOD_SHIFT, VK_NUMPAD0, None], 201: [MOD_SHIFT, VK_NUMPAD1, None], 202: [MOD_SHIFT, VK_NUMPAD2, None],
+        203: [MOD_SHIFT, VK_NUMPAD3, None], 204: [MOD_SHIFT, VK_NUMPAD4, None], 205: [MOD_SHIFT, VK_NUMPAD5, None],
+        206: [MOD_SHIFT, VK_NUMPAD6, None], 207: [MOD_SHIFT, VK_NUMPAD7, None], 208: [MOD_SHIFT, VK_NUMPAD8, None],
+        209: [MOD_SHIFT, VK_NUMPAD9, None],
+
+        210: [MOD_SHIFT, VK_DECIMAL, None], 211: [MOD_SHIFT, VK_ADD, None], 212: [MOD_SHIFT, VK_SUBTRACT, None],
+        213: [MOD_SHIFT, VK_MULTIPLY, None], 214: [MOD_SHIFT, VK_DIVIDE, None], 215: [MOD_SHIFT, VK_RETURN, None],
+
+        # Control
+        300: [MOD_CONTROL, VK_NUMPAD0, None], 301: [MOD_CONTROL, VK_NUMPAD1, None], 302: [MOD_CONTROL, VK_NUMPAD2, None],
+        303: [MOD_CONTROL, VK_NUMPAD3, None], 304: [MOD_CONTROL, VK_NUMPAD4, None], 305: [MOD_CONTROL, VK_NUMPAD5, None],
+        306: [MOD_CONTROL, VK_NUMPAD6, None], 307: [MOD_CONTROL, VK_NUMPAD7, None], 308: [MOD_CONTROL, VK_NUMPAD8, None],
+        309: [MOD_CONTROL, VK_NUMPAD9, None],
+
+        310: [MOD_CONTROL, VK_DECIMAL, None], 311: [MOD_CONTROL, VK_ADD, None], 312: [MOD_CONTROL, VK_SUBTRACT, None],
+        313: [MOD_CONTROL, VK_MULTIPLY, None], 314: [MOD_CONTROL, VK_DIVIDE, None], 315: [MOD_CONTROL, VK_RETURN, None],
+
+        # Alt
+        400: [MOD_ALT, VK_NUMPAD0, None], 401: [MOD_ALT, VK_NUMPAD1, None], 402: [MOD_ALT, VK_NUMPAD2, None],
+        403: [MOD_ALT, VK_NUMPAD3, None], 404: [MOD_ALT, VK_NUMPAD4, None], 405: [MOD_ALT, VK_NUMPAD5, None],
+        406: [MOD_ALT, VK_NUMPAD6, None], 407: [MOD_ALT, VK_NUMPAD7, None], 408: [MOD_ALT, VK_NUMPAD8, None],
+        409: [MOD_ALT, VK_NUMPAD9, None],
+
+        410: [MOD_ALT, VK_DECIMAL, None], 411: [MOD_ALT, VK_ADD, None], 412: [MOD_ALT, VK_SUBTRACT, None],
+        413: [MOD_ALT, VK_MULTIPLY, None], 414: [MOD_ALT, VK_DIVIDE, None], 415: [MOD_ALT, VK_RETURN, None],
     }
 
-    # 当相关热键被按下时，执行相关的命令，并进行相关提示
-    def handler(self, id):
-        # 获取按下快捷键时，当前剩余的秒数，以及需要进行的操作
-        common.seconds_sem.acquire()
-        seconds = common.seconds
-        operation = self.HotKeys.get(id)[2]
-        number = self.HotKeys.get(id)[3]
+    def mapping(self, mod, key, func):
+        mod_num = {'Win': 100, 'Shift': 200, 'Ctrl': 300, 'Alt': 400}
+        key_num = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '.': 10, '+': 11,
+                   '-': 12, '*': 13, '/': 14, '↵': 15}
+        map_num = mod_num[mod] + key_num[key]
+        print(map_num)
+        self.Mapping[map_num][2] = func
 
-        # 计算临时变量
-        if operation == "+":
-            seconds = seconds + number
-        elif operation == "-":
-            seconds = seconds - number
-            if seconds < 0: seconds = 0
-        elif operation == "=":
-            seconds = number
-            if seconds < 0: seconds = 0
+    def insert_callback(self, mod, key, func):
+        self.mapping(mod, key, func)
 
-        # 更新全局变量
-        common.seconds = seconds
-        common.seconds_sem.release()
+    def delete_callback(self, mod, key):
+        self.mapping(mod, key, self.f)
 
-        # 将更新告知用户
-        if seconds == 0: return
-        if number == 0: statement = "当前时间是："
-        else: statement = "时间已改为："
-        statement +=  common.seconds2str(seconds)
-        print(statement)
-        speaker.speak(statement)
+    # ------------------------------------------------------------------------------------------------------------------
 
     # 批量注册热键
     def register(self):
-        for id, (modifier, key, a, b) in self.HotKeys.items():
+        for id, (modifier, key, func) in self.Mapping.items():
             if not user32.RegisterHotKey(None, id, modifier, key):
-                print("热键： %s + %s 注册失败！" % (modifier, key))
+                common.log("热键注册失败！ID: %s, MOD: %s, KEY: %s" % (id, modifier, key), 30)
 
     # 批量取消注册过的热键，必须得释放热键，否则下次就会注册失败
     def unregister(self):
-        for id in self.HotKeys.keys():
+        for id in self.Mapping.keys():
             user32.UnregisterHotKey(None, id)
 
-    # 线程执行
+    # 当对应的热键检测到后
+    def handle(self, id):
+        f = self.Mapping[id][2]
+        if f: f()
+        else: common.log("一个未被注册的热键调用了", 20)
+
+    # 循环检测热键是否被触发
+    def listener(self):
+        msg = ctypes.wintypes.MSG()
+        while user32.GetMessageA(ctypes.byref(msg), None, 0, 0) != 0:
+            if msg.message == WM_HOTKEY:  # 当监测到有热键被按下时
+                self.handle(msg.wParam)
+            user32.TranslateMessage(ctypes.byref(msg))
+            user32.DispatchMessageA(ctypes.byref(msg))
+
     def run(self):
-
         self.register()
-
-        # 循环检测热键是否被按下
         try:
-            msg = ctypes.wintypes.MSG()
-            while user32.GetMessageA(ctypes.byref(msg), None, 0, 0) != 0:
-                if msg.message == WM_HOTKEY:  # 当监测到有热键被按下时
-                    self.handler(msg.wParam)  # msg.wParam 就是热键注册的ID
-                user32.TranslateMessage(ctypes.byref(msg))
-                user32.DispatchMessageA(ctypes.byref(msg))
+            self.listener()
         finally:
             self.unregister()
 
-def check():
-    pass
+hk = Hotkey()
+hk.setDaemon(True)
 
-def listen():
-    pass # todo: 热键的启动以后放到这里
+def start():
+    hk.start()
+
+def register(mod, key, func):
+    hk.mapping(mod, key, func)
--- readme.md
+++ readme.md
@@ -1,26 +1,26 @@
 ## 基础的交互操作
 
-| Key \ Modifier | Win -- System        | Shift -- Preset       | Ctrl -- Plus        | Alt -- Minus         |
+| Key \ Modifier | Win -- App           | Shift -- Preset       | Ctrl -- Plus        | Alt -- Minus         |
 |:--------------:|----------------------|-----------------------|---------------------|----------------------|
 |    Numpad 1    | Say a dictum         | Start 1st preset      | Plus 1 minutes      | Minus 1 minutes      |
 |    Numpad 2    | Talk a joke          | Start 2nd preset      | Plus 2 minutes      | Minus 2 minutes      |
 |    Numpad 3    | Read a poem          | Start 3rd preset      | Plus 3 minutes      | Minus 3 minutes      |
-|    Numpad 4    | Teach a idoim        | Start 4th preset      | Plus 4 minutes      | Minus 4 minutes      |
+|    Numpad 4    | Teach a idiom        | Start 4th preset      | Plus 4 minutes      | Minus 4 minutes      |
 |    Numpad 5    | Play a music         | Start 5th preset      | Plus 5 minutes      | Minus 5 minutes      |
 |    Numpad 6    | Play a crosstalk     | Start 6th preset      | Plus 6 minutes      | Minus 6 minutes      |
 |    Numpad 7    | Tell a story         | Start 7th preset      | Plus 7 minutes      | Minus 7 minutes      |
 |    Numpad 8    | Report current time  | Start 8th preset      | Plus 8 minutes      | Minus 8 minutes      |
-|    Numpad 9    | Report current count | Start 9th preset      | Plus 9 minutes      | Minus 9 minutes      |
-|    Numpad 0    | Repeat last sentence | Start **10th** preset | Plus **10** minutes | Minus **10** minutes |
+|    Numpad 9    | Report current total | Start 9th preset      | Plus 9 minutes      | Minus 9 minutes      |
+|    Numpad 0    | Repeat current count  | Start **10th** preset | Plus **10** minutes | Minus **10** minutes |
 
 
-| Key \ Modifier  | Win -- Voice             | Shift -- Counter           | Ctrl -- Tracer            | Alt -- Bomb             |
+| Key \ Modifier  | Win -- App               | Shift -- Counter           | Ctrl -- Tracer            | Alt -- Bomb             |
 |-----------------|--------------------------|----------------------------|---------------------------|-------------------------|
-| "." -- Report   | Report current Voice     | Report current Counter     | Report current Tracer     | Report current Bomb     |
-| "+" -- Next     | Switch to next Voice     | Switch to next Counter     | Switch to next Tracer     | Switch to next Bomb     |
-| "-" -- Previous | Switch to previous Voice | Switch to previous Counter | Switch to previous Tracer | Switch to previous Bomb |
-| "*" -- Random   | Switch to random Voice   | Switch to random Counter   | Switch to random Tracer   | Switch to random Bomb   |
-| "/" -- Clear    | Shutdown current Voice   | Shutdown current Counter   | Shutdown current Tracer   | Deactive current Bomb   |
+| "." -- Report   | Repeat last Sound        | Report current Counter     | Report current Tracer     | Report current Bomb     |
+| "+" -- Next     |                          | Switch to next Counter     | Switch to next Tracer     | Switch to next Bomb     |
+| "-" -- Previous |                          | Switch to previous Counter | Switch to previous Tracer | Switch to previous Bomb |
+| "*" -- Random   | Run a random APP         | Switch to random Counter   | Switch to random Tracer   | Switch to random Bomb   |
+| "/" -- Clear    |                          | Shutdown current Counter   | Shutdown current Tracer   | Deactive current Bomb   |
 | "↵" -- Edit     | Edit Voice configuration | Edit Preset configuration  | Edit Tracer configuration | Edit Bomb configuration |
 
 > Table edited with [Tables Generator](http://www.tablesgenerator.com/markdown_tables)
